645d8bf3b28297b69af55b56df966a100314f9b7
Edmund Leibert III <eleibert@ucsd.edu>
2023-04-24 03:26:09 -0700

    🩹 fix[create-links.ps1]: Fixed nvim config folder not being created in the nvims folder

--- create-links.ps1
+++ create-links.ps1
@@ -1,14 +1,17 @@
 # Get the name of the current folder and store it as a string; not the full path
 $nvimConfigProfile = Get-Location
-$nvimConfigProfile = $currentDirectory.Name
+$nvimConfigProfile = Split-Path $nvimConfigProfile -Leaf
 
 Write-Host "Running the script: $($MyInvocation.MyCommand.Name)"
 
-# Delete all files currently in the nvim folder
+# Delete all files currently in the relevant nvims config folder
 Remove-Item -Path "C:\\Users\\edmun\\AppData\\Local\\nvims\\$($nvimConfigProfile)\\*" -Recurse -Force
 
 # Get the current directory
 $currentDirectory = Get-Location
+
+# Create a directory in the nvims folder called $nvimConfigProfile, if the folder does not exist
+New-Item -ItemType Directory -Path "C:\\Users\\edmun\\AppData\\Local\\nvims\\$($nvimConfigProfile)"
 
 # Get all items in the current folder
 $CONFIG_ITEMS = Get-ChildItem -Path *
@@ -19,6 +22,7 @@ foreach ($CONFIG_ITEM in $CONFIG_ITEMS) {
 	# If object is the file init.lua, create a symbolic link
 	if($CONFIG_ITEM.Name -eq "init.lua") {
 		Write-Host "Creating a symbolic for the following files... $($CONFIG_ITEM.name)"
+		Write-Host "C:\\Users\\edmun\\AppData\\Local\\nvims\\$($nvimConfigProfile)\\$($CONFIG_ITEM.name)"
 		New-Item -ItemType SymbolicLink -Path "C:\\Users\\edmun\\AppData\\Local\\nvims\\$($nvimConfigProfile)\\$($CONFIG_ITEM.name)" -Target "$($currentDirectory.Path)\\$($CONFIG_ITEM.name)"
 	}
 	# If object is a folder and the name is lua, create a symbolic link
